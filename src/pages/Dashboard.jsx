eaed454736981024bcbe97abddb216b5fd157abd
Rthvik <rithvik.ravilla@gmail.com>
2025-06-16 00:28:59 -0500

    feat: file upload section in dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from 'react';
 import { Button } from '@mantine/core';
 import axios from 'axios';
 import { jwtDecode } from 'jwt-decode';
+import { FileInput } from '@mantine/core';
 
 const baseURL = import.meta.env.VITE_BACKEND_URL;
 
 function Dashboard() {
   const [user, setUser] = useState(null);
+  const [file, setFile] = useState(null);
 
   useEffect(() => {
     // Step 1: Check if token is in query params (from Google callback redirect)
@@ -65,6 +67,26 @@ function Dashboard() {
     }
   };
 
+  // const handleUpload = async () => {
+  //   if (!value) return;
+  
+  //   const formData = new FormData();
+  //   formData.append('file', value);
+  
+  //   try {
+  //     const res = await axios.post(`${baseURL}/api/upload`, formData, {
+  //       headers: {
+  //         'Content-Type': 'multipart/form-data',
+  //         Authorization: `Bearer ${localStorage.getItem('token')}`,
+  //       },
+  //       withCredentials: true,
+  //     });
+  //     console.log('Upload success:', res.data);
+  //   } catch (err) {
+  //     console.error('Upload failed:', err);
+  //   }
+  // };
+
   if (!user) return <div>Loading or not authenticated...</div>;
 
   return (
@@ -73,6 +95,14 @@ function Dashboard() {
       <Button onClick={handleLogout} color="red">
         Logout
       </Button>
+
+      <FileInput 
+        accept="application/pdf" 
+        label="Upload files" 
+        placeholder="Upload files" 
+        value={file}
+        onChange={setFile} 
+      />
     </div>
   );
 }
