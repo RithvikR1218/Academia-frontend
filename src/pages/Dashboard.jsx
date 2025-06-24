d55add229532340e7fb182a9ba2a6fd818d1d192
Gaurav Narayanan <105504951+GauravN0910@users.noreply.github.com>
2025-06-24 16:47:29 +0530

    feat: file upload (#8)
    Co-authored-by: GauravN0910 <root@Gaurav>

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,12 +3,15 @@ import { Button } from '@mantine/core';
 import axios from 'axios';
 import { jwtDecode } from 'jwt-decode';
 import { FileInput } from '@mantine/core';
+import UploadedFilesList from '../components/UploadedFiles';
 
 const baseURL = import.meta.env.VITE_BACKEND_URL;
 
 function Dashboard() {
   const [user, setUser] = useState(null);
   const [file, setFile] = useState(null);
+  const [uploading, setUploading] = useState(false);
+  const [refresh, setRefresh] = useState(false);
 
   useEffect(() => {
     // Step 1: Check if token is in query params (from Google callback redirect)
@@ -51,7 +54,7 @@ function Dashboard() {
         localStorage.removeItem('token');
         window.location.href = '/';
       });
-  }, []);
+  }, [refresh]);
 
   const handleLogout = async () => {
     try {
@@ -67,25 +70,47 @@ function Dashboard() {
     }
   };
 
-  // const handleUpload = async () => {
-  //   if (!value) return;
-  
-  //   const formData = new FormData();
-  //   formData.append('file', value);
-  
-  //   try {
-  //     const res = await axios.post(`${baseURL}/api/upload`, formData, {
-  //       headers: {
-  //         'Content-Type': 'multipart/form-data',
-  //         Authorization: `Bearer ${localStorage.getItem('token')}`,
-  //       },
-  //       withCredentials: true,
-  //     });
-  //     console.log('Upload success:', res.data);
-  //   } catch (err) {
-  //     console.error('Upload failed:', err);
-  //   }
-  // };
+  const handleUpload = async () => {
+    if(!file){
+      alert("Please select a file to upload.");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    setUploading(true);
+    const token = localStorage.getItem('token');
+
+    try {
+      const response = await fetch(`${baseURL}/api/auth/files/create`, {
+        method: 'POST',
+        body: formData,
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const data = await response.json();
+      console.log(response.status);
+      if (!response.ok) {
+        throw new Error(data.error || 'Upload failed');
+      }
+
+      console.log('File uploaded successfully:', data.url);
+      alert('Upload success!');
+    } catch (error) {
+      console.error('Upload failed:', error);
+      alert('Error uploading file.');
+    }
+    finally {
+      setUploading(false);
+      setFile(null); 
+      setRefresh(prev => !prev);
+    }
+  };
+
 
   if (!user) return <div>Loading or not authenticated...</div>;
 
@@ -103,6 +128,16 @@ function Dashboard() {
         value={file}
         onChange={setFile} 
       />
+      <Button 
+        onClick={handleUpload} 
+        color="blue" 
+        disabled={uploading || !file}
+        loading={uploading}
+      >
+        Upload
+      </Button>
+      <h2>Uploaded Files</h2>
+      <UploadedFilesList uploadedFiles={user.uploadedFiles} />
     </div>
   );
 }
