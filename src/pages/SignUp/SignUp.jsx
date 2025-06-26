395209e298ff1277b2b369a2648ce2298b8ed4fa
Gaurav Narayanan <105504951+GauravN0910@users.noreply.github.com>
2025-06-27 00:24:45 +0530

    Feat/local auth (#16)
    * feat: local signup and login
    
    * feat: css for uploaded files and local storage fix
    
    * fix: verifyOTP screen
    
    ---------
    
    Co-authored-by: GauravN0910 <root@Gaurav>

---
+++ src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,103 @@
+import './SignUp.css';
+import { useState, useEffect } from 'react';
+import { notifications } from '@mantine/notifications';
+const baseURL = import.meta.env.VITE_BACKEND_URL;
+
+
+export default function SignUp() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            handleSignUp();
+        }
+    };
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            window.location.href = '/dashboard';
+        }
+    }, []);
+
+    const handleGoogleLogin = () => {
+        window.location.href = `${baseURL}/api/auth/google`; 
+    };
+
+    const handleSignUp = async () => {
+        if (!email || !password || !name) {
+            notifications.show({
+            title: 'Error!',
+            message: 'Please fill in all fields.',
+            color: 'red',
+            });
+            return;
+        }
+
+        try {
+            const response = await fetch(`${baseURL}/api/auth/signup`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    displayName: name,
+                    email: email,
+                    password: password,
+                }),
+            });
+
+            if (response.ok) {
+                localStorage.setItem('email', email);
+                window.location.href = '/verify-otp';
+            } 
+            else {
+                const { error } = await response.json();
+                notifications.show({
+                    title: 'Error!',
+                    message: error || 'Sign-up failed.',
+                    color: 'red',
+                });
+            }
+        } catch (err) {
+            notifications.show({
+            title: 'Error!',
+            message: 'Something went wrong.',
+            color: 'red',
+            });
+        }
+    };
+
+    
+    return (
+        <div className="signup-container">
+            <div className="gradient-blob-top"></div>
+            <div className="gradient-blob-bottom"></div>
+            <div className="gradient-blob-footer"></div>
+            <div className="signup-card">
+                <h2 className="hero-h1">Sign Up</h2>
+                <div className="signup-bar">
+                    <input className="signup-input" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} onKeyDown={handleKeyDown}/>
+                </div>
+                <div className="signup-bar">
+                    <input className="signup-input" type='email' placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} onKeyDown={handleKeyDown}/>
+                </div>
+                <div className="signup-bar">
+                    <input className="signup-input" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} onKeyDown={handleKeyDown}/>
+                </div>
+
+                <button className="signup-button" onClick={handleSignUp}>
+                    Continue
+                </button>
+
+                <div className="divider"></div>
+
+                <button className="signup-button" onClick={handleGoogleLogin}>
+                    <i className="fa-brands fa-google"></i>&nbsp;Sign in with Google
+                </button>
+            </div>
+        </div>
+    );
+}
---
+++ src/pages/VerifyOTP/VerifyOTP.jsx
@@ -0,0 +1,112 @@
+import './VerifyOTP.css';
+import { useRef, useEffect } from 'react';
+import { notifications } from '@mantine/notifications';
+const baseURL = import.meta.env.VITE_BACKEND_URL;
+
+
+export default function VerifyOTP() {
+  const inputRefs = useRef([]);
+
+  useEffect(() => {
+    const storedEmail = localStorage.getItem('email');
+    if (!storedEmail) {
+        window.location.href = '/signup';
+    }
+  }, []);
+
+  const handleChange = (e, index) => {
+    const value = e.target.value;
+
+    if (/^\d$/.test(value)) {
+        e.target.value = value;
+        if (index < 5) {
+            inputRefs.current[index + 1].focus();
+        }
+    } 
+    else {
+        e.target.value = '';
+        inputRefs.current[index].focus();
+    }
+  };
+
+  const handleKeyDown = (e, index) => {
+    if (e.key === 'Backspace' && !e.target.value && index > 0) {
+      inputRefs.current[index - 1].focus();
+    }
+    if (e.key === 'Enter') {
+        handleVerify();
+    }
+  };
+
+  const handleVerify = async () => {
+    const otp = inputRefs.current.map(input => input.value).join('');
+    console.log('OTP entered:', otp); 
+
+    if (otp.length !== 6) {
+        alert('Please enter a valid 6-digit OTP');
+        return;
+    }
+
+    try {
+        const response = await fetch(`${baseURL}/api/auth/verifyOTP`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                otp,
+                email: localStorage.getItem('email'),
+            }),
+        });     
+
+        if(response.ok){
+            localStorage.removeItem('email');
+            window.location.href = '/login';
+        }
+        else {
+            const { error } = await response.json();
+            notifications.show({
+                title: 'Error!',
+                message: error || 'Login failed.',
+                color: 'red',
+            });
+        }
+    }
+    catch (err) {
+        notifications.show({
+            title: 'Error!',
+            message: 'Something went wrong.',
+            color: 'red',
+        });
+    }
+  };
+
+  return (
+    <div className="home-container">
+        <div className="gradient-blob-top"></div>
+        <div className="gradient-blob-bottom"></div>
+        <div className="gradient-blob-footer"></div>
+        <div className="login-card">
+            <h2 className="hero-h1">Enter OTP</h2>
+            <p className="otp-instructions">Verify the 6-digit code to your email</p>
+
+            <div className="otp-input-group">
+            {[...Array(6)].map((_, i) => (
+                <input
+                key={i}
+                type="text"
+                maxLength={1}
+                className="otp-input"
+                onChange={(e) => handleChange(e, i)}
+                onKeyDown={(e) => handleKeyDown(e, i)}
+                ref={(el) => (inputRefs.current[i] = el)}
+                />
+            ))}
+            </div>
+            <button className="login-button" onClick={handleVerify}>
+                Verify
+            </button>
+        </div>
+    </div>
+  );
+}
