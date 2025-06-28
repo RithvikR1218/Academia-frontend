1fa72656ba9f5fbe7b107f092fc7db2bd608d103
Kunal Hinduja <kunalhinduja2002@gmail.com>
2025-06-28 11:31:45 +0000

    feat: added functionality to make call to api gateway and also updated env example

--- src/components/SummariseBox/SummariseBox.jsx
+++ src/components/SummariseBox/SummariseBox.jsx
@@ -1,14 +1,58 @@
+import { useEffect, useState } from 'react';
+import { Loader } from '@mantine/core';
 import './SummariseBox.css';
 
-export default function SummariseBox({ fileName, onClose }) {
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
+export default function SummaryOverlay({ fileName, onClose }) {
+  const [summary, setSummary] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const waitForSummary = (fileName) => {
+      return new Promise((resolve) => {
+        const interval = setInterval(async () => {
+          try {
+            const res = await fetch(`${baseUrl}fileName=${fileName}`);//change this if required
+            const data = await res.json();
+            if (data.status === 'completed') {
+              clearInterval(interval);
+              clearTimeout(timeout);
+              resolve(data.summary);
+            }
+          } catch (err) {
+            console.error('Polling error:', err);
+          }
+        }, 5000);
+
+        const timeout = setTimeout(() => {
+          clearInterval(interval);
+          resolve('❌ Summary generation timed out.');
+        }, 180000);
+      });
+    };
+
+    waitForSummary(fileName).then((result) => {
+      setSummary(result);
+      setLoading(false);
+    });
+  }, [fileName]);
+
   return (
     <div className="summary-overlay">
       <div className="summary-content">
         <button className="summary-close-button" onClick={onClose}>
           ❌
         </button>
-        <h2>Summary for <span style={{ fontWeight: 500 }}>{fileName}</span></h2>
-        <p>This is your summary output.</p>
+        <h2>
+          Summary for <span style={{ fontWeight: 500 }}>{fileName}</span>
+        </h2>
+        {loading ? (
+          <div style={{ display: 'flex', justifyContent: 'center', marginTop: 20 }}>
+            <Loader color="blue" size="md" variant="dots" />
+          </div>
+        ) : (
+          <p>{summary}</p>
+        )}
       </div>
     </div>
   );
