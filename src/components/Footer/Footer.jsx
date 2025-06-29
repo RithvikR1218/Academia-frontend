16a194f19663b9e477f7260ab30071d3358659ac
NitroKnight68 <nish0602@gmail.com>
2025-06-30 00:55:47 +0530

    fix: changed footer logos

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import DockerLogo from '../../assets/Docker.png'
+import DynamoDBLogo from '../../assets/DynamoDB.png'
+import EC2Logo from '../../assets/EC2.png'
 import GatewayLogo from '../../assets/Gateway.png'
-import GoogleLogo from '../../assets/Google.png'
 import LambdaLogo from '../../assets/Lambda.png'
 import MongoDBLogo from '../../assets/MongoDB.png'
-import ReactLogo from '../../assets/React.png'
 import RedisLogo from '../../assets/Redis.png'
 import S3Logo from '../../assets/S3.png'
 import SESLogo from '../../assets/SES.png'
 import SQSLogo from '../../assets/SQS.png'
-import SwaggerLogo from '../../assets/Swagger.png'
+import TextractLogo from '../../assets/Textract.png'
 import TypesenseLogo from '../../assets/Typesense.png'
 import './Footer.css';
 
@@ -25,48 +25,48 @@ export default function Footer() {
                     <h3 className="techstack-h3">AWS Lambda</h3>
                 </div>
                 <div className="techstack-item">
-                    <img src={MongoDBLogo} alt="MongoDB" className="techstack-img" />
-                    <h3 className="techstack-h3">MongoDB</h3>
-                </div>
-                <div className="techstack-item">
-                    <img src={GoogleLogo} alt="Google OAuth" className="techstack-img" />
-                    <h3 className="techstack-h3">Google OAuth</h3>
-                </div>
-                <div className="techstack-item">
-                    <img src={RedisLogo} alt="Redis" className="techstack-img" />
-                    <h3 className="techstack-h3">Redis</h3>
+                    <img src={DynamoDBLogo} alt="AWS DynamoDB" className="techstack-img" />
+                    <h3 className="techstack-h3">AWS DynamoDB</h3>
                 </div>
                 <div className="techstack-item">
                     <img src={SQSLogo} alt="AWS SQS" className="techstack-img" />
                     <h3 className="techstack-h3">AWS SQS</h3>
                 </div>
                 <div className="techstack-item">
-                    <img src={SwaggerLogo} alt="Swagger" className="techstack-img" />
-                    <h3 className="techstack-h3">Swagger</h3>
+                    <img src={TextractLogo} alt="AWS Textract" className="techstack-img" />
+                    <h3 className="techstack-h3">AWS Textract</h3>
+                </div>
+                <div className="techstack-item">
+                    <img src={MongoDBLogo} alt="MongoDB" className="techstack-img" />
+                    <h3 className="techstack-h3">MongoDB</h3>
+                </div>
+                <div className="techstack-item">
+                    <img src={RedisLogo} alt="Redis" className="techstack-img" />
+                    <h3 className="techstack-h3">Redis</h3>
                 </div>
                 <div className="techstack-item">
                     <img src={GatewayLogo} alt="AWS API Gateway" className="techstack-img" />
                     <h3 className="techstack-h3">AWS API Gateway</h3>
                 </div>
                 <div className="techstack-item">
-                    <img src={TypesenseLogo} alt="Typesense" className="techstack-img" />
-                    <h3 className="techstack-h3">Typesense</h3>
-                </div>
-                <div className="techstack-item">
                     <img src={S3Logo} alt="AWS S3" className="techstack-img" />
                     <h3 className="techstack-h3">AWS S3</h3>
-                </div>
-                <div className="techstack-item">
-                    <img src={DockerLogo} alt="Docker" className="techstack-img" />
-                    <h3 className="techstack-h3">Docker</h3>
                 </div>
                 <div className="techstack-item">
                     <img src={SESLogo} alt="AWS SES" className="techstack-img" />
                     <h3 className="techstack-h3">AWS SES</h3>
                 </div>
                 <div className="techstack-item">
-                    <img src={ReactLogo} alt="React" className="techstack-img" />
-                    <h3 className="techstack-h3">React</h3>
+                    <img src={EC2Logo} alt="AWS EC2" className="techstack-img" />
+                    <h3 className="techstack-h3">AWS EC2</h3>
+                </div>
+                <div className="techstack-item">
+                    <img src={TypesenseLogo} alt="Typesense" className="techstack-img" />
+                    <h3 className="techstack-h3">Typesense</h3>
+                </div>
+                <div className="techstack-item">
+                    <img src={DockerLogo} alt="Docker" className="techstack-img" />
+                    <h3 className="techstack-h3">Docker</h3>
                 </div>
             </div>
         </div>
