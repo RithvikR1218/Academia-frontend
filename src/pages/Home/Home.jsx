f9db0bb09a60c8f7352a4fd7335a599e15e3d189
GauravN0910 <root@Gaurav>
2025-06-30 22:20:56 +0530

    chore: added home page content

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,29 +10,28 @@ export default function Home() {
         <p className="hero-p">For Research Driven Students</p>
         <h1 className="hero-h1">Discover the Best Professors Quickly</h1>
         <div className="about-section">
-            <p className="about-p">About Us</p>
             <h2 className="about-h2">Key Features</h2>
             <div className="features-section">
                 <div className="feature-div">
                     <div className="feature-header">
                         <span className="feature-span"><i class="fa-solid fa-caret-right"></i></span>
-                        <h3 className="feature-h3">First Thing</h3>
+                        <h3 className="feature-h3">Professor Search</h3>
                     </div>
-                    <p className="feature-p">Lorem ipsum dolor sit, consectetur neque porro quisquam est</p>
+                    <p className="feature-p">Find professors around the world and filter by their College, Department, or Research Interests.</p>
                 </div>
                 <div className="feature-div">
                     <div className="feature-header">
                         <span className="feature-span"><i class="fa-solid fa-caret-right"></i></span>
-                        <h3 className="feature-h3">Second Thing</h3>
+                        <h3 className="feature-h3">Personalized Dashboard</h3>
                     </div>
-                    <p className="feature-p">Lorem ipsum dolor sit, consectetur neque porro quisquam est</p>
+                    <p className="feature-p">Access a user-specific dashboard to save professor profiles, organize research files, and track your progress.</p>
                 </div>
                 <div className="feature-div">
                     <div className="feature-header">
                         <span className="feature-span"><i class="fa-solid fa-caret-right"></i></span>
-                        <h3 className="feature-h3">Third Thing</h3>
+                        <h3 className="feature-h3">Paper Summarizations</h3>
                     </div>
-                    <p className="feature-p">Lorem ipsum dolor sit, consectetur neque porro quisquam est</p>
+                    <p className="feature-p">Get summaries of professors' recent papers to understand their research and write cold emails more effectively.</p>
                 </div>
             </div>
         </div>
